4c6fb5bee6a7353a6c72dcfd15d78062ae5537cd
Jorge Martinez <jorgemartinez@Jorges-MBP.fritz.box>
2021-02-14 00:31:15 +0100

    added grid with circle object

--- sketch.js
+++ sketch.js
@@ -1,106 +1,146 @@
-let darkPurple = '#352D39'
-let pink = '#FF6978'
+let img
+let uploadImg
+const cols = 35
+const rows = 35
+let matrix = []
+let xSpacing
+let ySpacing
+let w
+let h
+let input;
 
-const between = (x, min, max) =>  x >= min && x <= max
-
-function isOverGridPoint (mouseX, mouseY, x, y, range = 20) {
-  return (between(mouseX, x-range, x+range) && between(mouseY, y-range, y+range))
-    ? true
-    : false
+function preload() {
+  img = loadImage('https://i.imgur.com/6B788ty.jpg')
 }
 
-// https://coolors.co/ff6978-fffcf9-b1ede8-6d435a-352d39
-  const windowResized = (p5) => resizeCanvas(windowWidth, windowHeight)
-  const lineThresh = 40
-  let color = pink
-  let spots = []
-  let cols = 20
-  let canvas
-  let rows
+function setup() {
+  image(uploadImg ? uploadImg : img, 0, 0)
+  var myCanvas = createCanvas(img.width, img.height)
+  w = width/cols
+  h = height/rows
+  input = createFileInput(handleFile);
+  // myCanvas.parent("p5")
 
-  function Spot(p5, x, y) {
-    let _x = x
-    let _y = y
-    let _r = 2
-    let hovered = false
+  xSpacing = width/cols
+  ySpacing = height/cols
 
-    function display() {
-      noStroke()
-      fill(color)
-      ellipse(_x, _y, _r)
-    }
-
-    function fade() {
-      if (_r >= 0) {
-        _r -= 1
-      } else {
-        hovered = false
-      }
-    }
-
-    function flash() {
-        _r = 20
-        hovered = true
-    }
-
-    function randomWalk() {
-      _x += random(-0.5,0.5)
-      _y += random(-0.5,0.5)
-    }
-
-    function values () {
-      return {
-        _x,
-        _y,
-        hovered,
-      }
-    }
-
-    return {
-      display,
-      flash,
-      randomWalk,
-      values,
-      fade,
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      matrix.push([Spot(i*xSpacing,j*ySpacing)])
     }
   }
 
-  function setup() {
-    canvas = createCanvas(windowWidth, windowHeight /*, WEBGL*/)
-    background(darkPurple);
-    canvas.position(0, 0).style('z-index', '-1')
-    
-    cols = map(width, 0, 2000 , 2, 30)
-    const gridSize  = width/cols
-    rows = height/gridSize
+  saveColors(matrix)
+}
 
-    for (let x = 0; x < cols+1; x++) {
-      for (let y = 0; y < rows+1; y++) {
-        spots.push(Spot(p5, x * gridSize, y * gridSize))
-      }
-    }
-  }
+function saveColors(matrix) {
+  matrix.forEach((cell, i) => {
+    matrix[i].push(get(cell[0].values()._x, cell[0].values()._y))
+  })    
+}
+  
+function draw() {
+  uploadImg && resizeCanvas(uploadImg.width, uploadImg.height);
 
-  function draw () {
-    background(darkPurple)
-    strokeWeight(1)
-    spots.forEach((s, i) => {
-      let { _x, _y, hovered } = s.values()
-      s.randomWalk()
-      s.display()
-      if (i !== 0 && isOverGridPoint(mouseX, mouseY, _x, _y)) s.flash() 
-      hovered && s.fade()
+  // let newCols = Math.ceil(map(mouseX, 0, width, 0, 40))
+  // let newRows = Math.ceil(map(mouseY, 0, height, 0, 40))
 
-      spots.forEach(ref => {
-        if (between(_x, ref.values()._x-lineThresh, ref.values()._x+lineThresh) && between(_y, ref.values()._y-lineThresh, ref.values()._y+lineThresh)) {
-          stroke(color)
-          strokeWeight(1)
-          line(_x, _y, ref.values()._x, ref.values()._y)
-        }
-      })
-    })
-  }
-
-  // mouseClicked = () => {
-  //   console.log(8)
+  // If mouse is inside the canvas:
+  // if (mouseX > 1 && mouseX < width && mouseY > 1 && mouseY < height) {
+  //   w = width/newCols
+  //   h = height/newRows
+  //   matrix = buildNewMatrix(newCols, newRows, width/newCols, height/newRows)
+  // } else {
+  //   w = width/cols
+  //   h = height/rows
+  //   matrix = buildNewMatrix(cols, rows, width/cols, height/rows)
   // }
+
+  // image(uploadImg ? uploadImg : img, 0, 0)
+  // saveColors(matrix)
+  background(0)
+  // shuffleArray(matrix)
+
+  matrix.forEach((cell, i) => {
+    debugger
+    fill(cell[1])
+    noStroke()
+    cell[0].display()
+    // ellipseMode(CORNER)
+    // ellipse(cell[0], cell[1], w, h)
+  })  
+}
+
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+// function buildNewMatrix(rows, cols, xS, yS) {
+//   let m = []
+//   for (let i = 0; i < rows+1; i++) {
+//     for (let j = 0; j < cols+1; j++) {
+//       m.push([i*xS,j*yS])
+//     }
+//   }
+//   return m
+// }
+
+function handleFile(file) {
+  print(file);
+  if (file.type === 'image') {
+    uploadImg = createImg(file.data, '');
+    uploadImg.hide();
+  } else {
+    uploadImg = null;
+  }
+}  
+
+function Spot(x, y) {
+  let _x = x
+  let _y = y
+  let _r = 2
+  let hovered = false
+
+  function display() {
+    noStroke()
+    fill(50)
+    ellipse(_x, _y, _r)
+  }
+
+  // function fade() {
+  //   if (_r >= 0) {
+  //     _r -= 1
+  //   } else {
+  //     hovered = false
+  //   }
+  // }
+
+  // function flash() {
+  //     _r = 20
+  //     hovered = true
+  // }
+
+  // function randomWalk() {
+  //   _x += random(-0.5,0.5)
+  //   _y += random(-0.5,0.5)
+  // }
+
+  function values () {
+    return {
+      _x,
+      _y,
+      hovered,
+    }
+  }
+
+  return {
+    display,
+    // flash,
+    // randomWalk,
+    values,
+    // fade,
+  }
+}
