3f5efe3eafed28754a78db374a114b457cab62fa
Jorge Martinez <jorgemartinez@Jorges-MBP.fritz.box>
2021-02-26 23:33:52 +0100

    got it working

--- sketch.js
+++ sketch.js
@@ -9,17 +9,11 @@ let w
 let h
 let input;
 
-function preload() {
-  img = loadImage('https://i.imgur.com/6B788ty.jpg')
-}
-
 function setup() {
   noLoop()
-  var myCanvas = createCanvas(img.width, img.height)
-  image(uploadImg ? uploadImg : img, 0, 0)
+  var myCanvas = createCanvas(600,600)
   w = width/cols
   h = height/rows
-  // input = createFileInput(handleFile);
   // myCanvas.parent("p5")
   xSpacing = width/cols
   ySpacing = height/cols
@@ -27,15 +21,13 @@ function setup() {
   for (let i = 0; i < rows; i++) {
     for (let j = 0; j < cols; j++) {
       matrix.push([
-        Spot(i*xSpacing,j*ySpacing, xSpacing, ySpacing),
-        get(i*xSpacing,j*ySpacing)
+        Spot(i*xSpacing,j*ySpacing, xSpacing, ySpacing)
       ])
     }
   }
 }
 
 function draw() {
-  uploadImg && resizeCanvas(uploadImg.width, uploadImg.height);
 
   // let newCols = Math.ceil(map(mouseX, 0, width, 0, 40))
   // let newRows = Math.ceil(map(mouseY, 0, height, 0, 40))
@@ -59,15 +51,8 @@ function draw() {
 
   matrix.forEach((cell, i) => {
     noStroke()
-    cell[0].display(cell[1])
+    cell[0].display()
   })  
-}
-
-function shuffleArray(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
-  }
 }
 
 // function buildNewMatrix(rows, cols, xS, yS) {
@@ -80,33 +65,44 @@ function shuffleArray(array) {
 //   return m
 // }
 
-function handleFile(file) {
-  print(file);
-  if (file.type === 'image') {
-    uploadImg = createImg(file.data, '');
-    uploadImg.hide();
-  } else {
-    uploadImg = null;
+function randomDeg(_x, _y, w, h) {
+  let randomNumber = (Math.random() * 10).toFixed(1)
+  console.log('executed with: ', randomNumber)
+  if (randomNumber < 2.5 && randomNumber < 0) {
+    console.log(1)
+    return 0
+  } else if (randomNumber < 5 && randomNumber < 2.5) {
+    console.log(2)
+    return HALF_PI
+  } else if (randomNumber < 7.5 && randomNumber < 5) {
+    console.log(3)
+    return PI
+  } else if (randomNumber < 1.1 && randomNumber > 7.5) {
+    console.log(4)
+    return HALF_PI + PI
   }
-}  
-
-function maybe() {
-  return random() < 0.1
 }
 
 function Spot(x, y, w, h) {
+
   let _x = x
   let _y = y
   let _r = 10
   let hovered = false
 
-  function display(col) {
+  function display() {
     noStroke()
-    fill(col)
-    ellipseMode(CORNER)
-    // ellipse(_x, _y, w, h)
-    arc(_x, _y, w, h, 0, PI + QUARTER_PI, CHORD);
+    fill(random(255))
 
+    push()
+      translate(_x,_y)
+      let r = randomDeg()
+      // console.log(Math.random())
+      // console.log({r})
+      rotate(r)
+      arc(0, 0, w, h, 0, HALF_PI)
+      translate(0,0)
+    pop()
     // maybe() && rect(_x+5, _y+5, 20)
   }
 
