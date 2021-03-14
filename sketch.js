12e66e9803b2b18c5dcb8db4bec27be929b50688
GiorgioMartini <jorgeantonio82@googlemail.com>
2021-03-14 21:10:03 +0100

    final mona

--- sketch.js
+++ sketch.js
@@ -1,7 +1,8 @@
+
 let img
 let uploadImg
-const cols = 20
-const rows = 20
+const cols = 35
+const rows = 35
 let matrix = []
 let xSpacing
 let ySpacing
@@ -14,53 +15,58 @@ function preload() {
 }
 
 function setup() {
-  noLoop()
   var myCanvas = createCanvas(img.width, img.height)
-  image(uploadImg ? uploadImg : img, 0, 0)
   w = width/cols
   h = height/rows
-  // input = createFileInput(handleFile);
   // myCanvas.parent("p5")
+
   xSpacing = width/cols
   ySpacing = height/cols
 
   for (let i = 0; i < rows; i++) {
     for (let j = 0; j < cols; j++) {
-      matrix.push([
-        Spot(i*xSpacing,j*ySpacing, xSpacing, ySpacing),
-        get(i*xSpacing,j*ySpacing)
-      ])
+      matrix.push([i*xSpacing,j*ySpacing])
     }
   }
 }
 
+function saveColors(matrix) {
+  matrix.forEach((cell, i) => {
+    matrix[i].push(get(cell[0], cell[1]))
+  })    
+}
+  
 function draw() {
-  uploadImg && resizeCanvas(uploadImg.width, uploadImg.height);
+  let _newCols = map(mouseX, 0, width, 0, 40)
+  let newCols = Math.ceil(_newCols)
+  let _newRows = map(mouseY, 0, height, 0, 40)
+  let newRows = Math.ceil(_newRows)
 
-  // let newCols = Math.ceil(map(mouseX, 0, width, 0, 40))
-  // let newRows = Math.ceil(map(mouseY, 0, height, 0, 40))
-
-  // If mouse is inside the canvas:
-  // if (mouseX > 1 && mouseX < width && mouseY > 1 && mouseY < height) {
-  //   w = width/newCols
-  //   h = height/newRows
-  //   matrix = buildNewMatrix(newCols, newRows, width/newCols, height/newRows)
-  // } else {
-  //   w = width/cols
-  //   h = height/rows
-  //   matrix = buildNewMatrix(cols, rows, width/cols, height/rows)
-  // }
-
-  // image(uploadImg ? uploadImg : img, 0, 0)
-  // saveColors(matrix)
-  // background(0)
-  // shuffleArray(matrix)
+  if (mouseX > 1 && mouseX < width && mouseY > 1 && mouseY < height) {
+    w = width/newCols
+    h = height/newRows
+    matrix = buildNewMatrix(newCols, newRows, width/newCols, height/newRows)
+  } else {
+    w = width/cols
+    h = height/rows
+    matrix = buildNewMatrix(cols, rows, width/cols, height/rows)
+  }
+  console.log(1)
+  image(img, 0, 0)
+  console.log(2)
+  console.log({matrix})
+  saveColors(matrix)
+  console.log(3)
   background(0)
+  shuffleArray(matrix)
 
   matrix.forEach((cell, i) => {
+    fill(cell[2])
     noStroke()
-    cell[0].display(cell[1])
+    ellipseMode(CORNER)
+    ellipse(cell[0], cell[1], w, h)
   })  
+
 }
 
 function shuffleArray(array) {
@@ -70,77 +76,12 @@ function shuffleArray(array) {
   }
 }
 
-// function buildNewMatrix(rows, cols, xS, yS) {
-//   let m = []
-//   for (let i = 0; i < rows+1; i++) {
-//     for (let j = 0; j < cols+1; j++) {
-//       m.push([i*xS,j*yS])
-//     }
-//   }
-//   return m
-// }
-
-function handleFile(file) {
-  print(file);
-  if (file.type === 'image') {
-    uploadImg = createImg(file.data, '');
-    uploadImg.hide();
-  } else {
-    uploadImg = null;
-  }
-}  
-
-function maybe() {
-  return random() < 0.1
-}
-
-function Spot(x, y, w, h) {
-  let _x = x
-  let _y = y
-  let _r = 10
-  let hovered = false
-
-  function display(col) {
-    noStroke()
-    fill(col)
-    ellipseMode(CORNER)
-    // ellipse(_x, _y, w, h)
-    arc(_x, _y, w, h, 0, PI + QUARTER_PI, CHORD);
-
-    // maybe() && rect(_x+5, _y+5, 20)
-  }
-
-  // function fade() {
-  //   if (_r >= 0) {
-  //     _r -= 1
-  //   } else {
-  //     hovered = false
-  //   }
-  // }
-
-  // function flash() {
-  //     _r = 20
-  //     hovered = true
-  // }
-
-  // function randomWalk() {
-  //   _x += random(-0.5,0.5)
-  //   _y += random(-0.5,0.5)
-  // }
-
-  function values () {
-    return {
-      _x,
-      _y,
-      hovered,
+function buildNewMatrix(rows, cols, xS, yS) {
+  let m = []
+  for (let i = 0; i < rows+1; i++) {
+    for (let j = 0; j < cols+1; j++) {
+      m.push([i*xS,j*yS])
     }
   }
-
-  return {
-    display,
-    // flash,
-    // randomWalk,
-    values,
-    // fade,
-  }
+  return m
 }
