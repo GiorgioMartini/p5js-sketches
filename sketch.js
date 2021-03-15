8efba8900c9ea3787dd02735e54a33b3419237a2
GiorgioMartini <jorgeantonio82@googlemail.com>
2021-03-15 23:11:17 +0100

    Update sketch.js
    added dancing lines

--- sketch.js
+++ sketch.js
@@ -1,106 +1,66 @@
-let darkPurple = '#352D39'
-let pink = '#FF6978'
+let dancingLine2
 
-const between = (x, min, max) =>  x >= min && x <= max
+function isOdd(num) { return num % 2;}
 
-function isOverGridPoint (mouseX, mouseY, x, y, range = 20) {
-  return (between(mouseX, x-range, x+range) && between(mouseY, y-range, y+range))
-    ? true
-    : false
+function setup() {
+  createCanvas(windowWidth,windowHeight)
+  background("#18020C")
+  dancingLine1 = makeDancingLine()
+  dancingLine2 = makeDancingLine(true)
 }
 
-// https://coolors.co/ff6978-fffcf9-b1ede8-6d435a-352d39
-  const windowResized = (p5) => resizeCanvas(windowWidth, windowHeight)
-  const lineThresh = 40
-  let color = pink
-  let spots = []
-  let cols = 20
-  let canvas
-  let rows
+function draw() {
+  background("#18020C")
+  translate(width/2, height/2)
+  dancingLine1.display()
+  dancingLine2.display()
+}
 
-  function Spot(p5, x, y) {
-    let _x = x
-    let _y = y
-    let _r = 2
-    let hovered = false
+function makeDancingLine(flip) {
+  let history = []
+  let linesAmt =60
+  let col = 255
+  let v1 = createVector(0,0)
+  let v2 = createVector(0,0)
+  
+  let x1Angle = flip ? 90 : 0
+  let y1Angle = flip ? 90 : 0
+  let x2Angle = flip ? 90 : 0
+  let y2Angle = flip ? 90 : 0
 
-    function display() {
-      noStroke()
-      fill(color)
-      ellipse(_x, _y, _r)
-    }
+  let x1AngleOff = 0.03
+  let y1AngleOff = 0.002
+  let x2AngleOff = 0.015
+  let y2AngleOff = 0.02  
+  
+  let r1 = flip ? 250 : 300 
+  let r2 = flip ? 250 : 300 
 
-    function fade() {
-      if (_r >= 0) {
-        _r -= 1
-      } else {
-        hovered = false
-      }
-    }
+  function display() {
+    v1.set(r1 * sin(x1Angle), r1 * sin(y1Angle))
+    v2.set(r2 * sin(x2Angle), r2 * sin(y2Angle))
 
-    function flash() {
-        _r = 20
-        hovered = true
-    }
-
-    function randomWalk() {
-      _x += random(-0.5,0.5)
-      _y += random(-0.5,0.5)
-    }
-
-    function values () {
-      return {
-        _x,
-        _y,
-        hovered,
-      }
-    }
-
-    return {
-      display,
-      flash,
-      randomWalk,
-      values,
-      fade,
-    }
-  }
-
-  function setup() {
-    canvas = createCanvas(windowWidth, windowHeight /*, WEBGL*/)
-    background(darkPurple);
-    canvas.position(0, 0).style('z-index', '-1')
+    history.push([v1.x, v1.y, v2.x, v2.y]) 
     
-    cols = map(width, 0, 2000 , 2, 30)
-    const gridSize  = width/cols
-    rows = height/gridSize
+    if (history.length > linesAmt) history.splice(0,1)
+    
+    col = flip
+      ? "#E5FFDE"
+      : "#634B66"
 
-    for (let x = 0; x < cols+1; x++) {
-      for (let y = 0; y < rows+1; y++) {
-        spots.push(Spot(p5, x * gridSize, y * gridSize))
-      }
-    }
-  }
-
-  function draw () {
-    background(darkPurple)
-    strokeWeight(1)
-    spots.forEach((s, i) => {
-      let { _x, _y, hovered } = s.values()
-      s.randomWalk()
-      s.display()
-      if (i !== 0 && isOverGridPoint(mouseX, mouseY, _x, _y)) s.flash() 
-      hovered && s.fade()
-
-      spots.forEach(ref => {
-        if (between(_x, ref.values()._x-lineThresh, ref.values()._x+lineThresh) && between(_y, ref.values()._y-lineThresh, ref.values()._y+lineThresh)) {
-          stroke(color)
-          strokeWeight(1)
-          line(_x, _y, ref.values()._x, ref.values()._y)
-        }
-      })
+    history.forEach((linea, i) => {
+      stroke(col) 
+      strokeWeight(2)
+      line(linea[0],linea[1],linea[2],linea[3])
     })
+
+    x1Angle += flip ? x1AngleOff : x1AngleOff + 0.01
+    y1Angle += flip ? y1AngleOff : y1AngleOff + 0.01
+    x2Angle += flip ? x2AngleOff : x2AngleOff + 0.01
+    y2Angle += flip ? y2AngleOff : y2AngleOff + 0.01
   }
 
-  // mouseClicked = () => {
-  //   console.log(8)
-  // }
+  return {
+    display,
+  }
+}
