ef73daf5263b1ee688598b78288c7c33552caffc
GiorgioMartini <jorgeantonio82@googlemail.com>
2021-03-15 23:26:07 +0100

    Update sketch.js
    added binaryrain

--- sketch.js
+++ sketch.js
@@ -1,106 +1,95 @@
-let darkPurple = '#352D39'
-let pink = '#FF6978'
+let canvasHeight = 600
+let canvasWidth = 400
+let row
+let grid
+let amtOfHorizontalCircles = 20
+let linesAmt = 50
+let ySpacing = 10
+let lineSpacing = canvasHeight/linesAmt
+let circleMaxWidth = 10
+const interval = 500
+const circleSizeMultiplier = 9 
+const circleSizeProbability = 0.6   
 
-const between = (x, min, max) =>  x >= min && x <= max
-
-function isOverGridPoint (mouseX, mouseY, x, y, range = 20) {
-  return (between(mouseX, x-range, x+range) && between(mouseY, y-range, y+range))
-    ? true
-    : false
+function setup () {
+  createCanvas(canvasWidth, canvasHeight, WEBGL)
+  smooth() 
+  background("#140c28")
+  grid = createGrid([], linesAmt, lineSpacing)
+  setInterval(slideRow, interval)
 }
 
-// https://coolors.co/ff6978-fffcf9-b1ede8-6d435a-352d39
-  const windowResized = (p5) => resizeCanvas(windowWidth, windowHeight)
-  const lineThresh = 40
-  let color = pink
-  let spots = []
-  let cols = 20
-  let canvas
-  let rows
-
-  function Spot(p5, x, y) {
-    let _x = x
-    let _y = y
-    let _r = 2
-    let hovered = false
-
-    function display() {
-      noStroke()
-      fill(color)
-      ellipse(_x, _y, _r)
-    }
-
-    function fade() {
-      if (_r >= 0) {
-        _r -= 1
-      } else {
-        hovered = false
-      }
-    }
-
-    function flash() {
-        _r = 20
-        hovered = true
-    }
-
-    function randomWalk() {
-      _x += random(-0.5,0.5)
-      _y += random(-0.5,0.5)
-    }
-
-    function values () {
-      return {
-        _x,
-        _y,
-        hovered,
-      }
-    }
-
-    return {
-      display,
-      flash,
-      randomWalk,
-      values,
-      fade,
-    }
-  }
-
-  function setup() {
-    canvas = createCanvas(windowWidth, windowHeight /*, WEBGL*/)
-    background(darkPurple);
-    canvas.position(0, 0).style('z-index', '-1')
-    
-    cols = map(width, 0, 2000 , 2, 30)
-    const gridSize  = width/cols
-    rows = height/gridSize
-
-    for (let x = 0; x < cols+1; x++) {
-      for (let y = 0; y < rows+1; y++) {
-        spots.push(Spot(p5, x * gridSize, y * gridSize))
-      }
-    }
-  }
-
-  function draw () {
-    background(darkPurple)
-    strokeWeight(1)
-    spots.forEach((s, i) => {
-      let { _x, _y, hovered } = s.values()
-      s.randomWalk()
-      s.display()
-      if (i !== 0 && isOverGridPoint(mouseX, mouseY, _x, _y)) s.flash() 
-      hovered && s.fade()
-
-      spots.forEach(ref => {
-        if (between(_x, ref.values()._x-lineThresh, ref.values()._x+lineThresh) && between(_y, ref.values()._y-lineThresh, ref.values()._y+lineThresh)) {
-          stroke(color)
-          strokeWeight(1)
-          line(_x, _y, ref.values()._x, ref.values()._y)
-        }
-      })
+function draw () {
+  rotateX(6.7)
+  translate((-canvasWidth/2)+10, -canvasHeight/2, 100)
+  background("#140c28")
+  grid.forEach( row => {
+    row.forEach( x => {
+      x.display()
     })
-  }
+  })
+}
 
-  // mouseClicked = () => {
-  //   console.log(8)
-  // }
+
+const createCircle = (fillColor, x, y, maxRadius) => {
+let xpos = x
+let ypos = y
+ return {
+   xpos,
+   ypos,
+   display() {
+     noStroke()
+     fill(fillColor)
+     ellipse(xpos, ypos, maxRadius, maxRadius)
+   },
+   slide(amt) {
+     if( ypos >  linesAmt * lineSpacing ) {
+       maxRadius = randomCircleSize(circleSizeMultiplier, circleSizeProbability)
+       ypos = lineSpacing
+     }
+     ypos += amt
+   },
+ }
+}
+
+function randomLevelFromHsl(h, s, min, max) {
+  // Probability of color being more lit
+  const r = random(1)
+  if( r > 0.7 ) { 
+    return `hsl(${h}, ${s}%, ${random(min, max).toFixed()}%)`   
+  }
+  return `hsl(${h}, ${s}%, ${random(min/2, max/2).toFixed()}%)` 
+}
+
+function randomCircleSize(multiplier, probabilityToBeSmall/* e.g. 0.7 */) {
+  // Probability of size
+  const r = random(1)
+  if( r < probabilityToBeSmall ) { 
+    return r * multiplier/2
+  }
+  return r * multiplier
+}
+
+function createLine(arr, amt, startingPoint, y) {
+  if( arr.length < amt) {
+     arr.push(createCircle(randomLevelFromHsl(20, 80, 20, 50), startingPoint,  y, randomCircleSize(circleSizeMultiplier, circleSizeProbability) ))
+     createLine(arr, amt, startingPoint += width/amtOfHorizontalCircles, y)
+  }
+  return arr
+}
+
+function createGrid(arr, linesAmt, y) {
+  if( arr.length < linesAmt) {
+    arr.push(createLine([], amtOfHorizontalCircles, 0, y))
+    createGrid(arr, linesAmt, y += lineSpacing)} 
+  return arr
+}
+
+function slideRow() {
+  // shifts heights, and moves y's to begginig of they are higher tha Height
+  grid.forEach( (line) => {
+    line.forEach( x => {
+    x.slide(ySpacing)
+    })
+  })
+}
